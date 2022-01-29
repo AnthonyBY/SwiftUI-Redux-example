80ec9d4c03cfb615d3154195a3ef0a545154b161
Anton Marchanka <amarchan@partner.jaguarlandrover.com>
2022-01-30 00:23:11 +0300

    Add first action and reducer, update store model to include action as well

--- ThreeDucks/State/Actions.swift
+++ ThreeDucks/State/Actions.swift
@@ -32,17 +32,6 @@
 
 import Foundation
 
-typealias ThreeDucksStore = Store<ThreeDucksState, ThreeDucksAction>
-
-class Store<State, Action>: ObservableObject {
-  @Published private(set) var state: State
-  private let reducer: Reducer<State, Action>
-
-  init(
-    initial: State,
-    reducer: @escaping Reducer<State, Action>
-  ) {
-    self.state = initial
-    self.reducer = reducer
-  }
+enum ThreeDucksAction {
+  case startGame
 }
